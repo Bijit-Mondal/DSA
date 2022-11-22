931afd87ecc5b550321a78b3b7ad8064c49e0c7f
Bijit Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2022-11-22 09:41:57 +0000

    My Commit

--- tmp/ShellSort.c
+++ tmp/ShellSort.c
@@ -2,19 +2,31 @@
 #include<stdlib.h>
 int main(){
 	printf("Enter size of array ");
-	int size;
-	scanf("%d",&size);
-	int *arr =(int*)malloc(sizeof(int)*size);
-	for(int i=0;i<size;i++){
+	int n;
+	scanf("%d",&n);
+	int *arr =(int*)malloc(sizeof(int)*n);
+	for(int i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	}
 	int j,key;
-	for(int gap=size/2;gap>0;gap=gap/2){
-		for(int i=gap;i<size;i++){
-			key = arr[i];
-			for(j=gap-i;j>=0 && key<arr[j-i];j-=i){
+	for(int gap=n/2;gap>0;gap=gap/2){
+		// for(int i=gap;i<size;i++){
+		// 	key = arr[i];
+		// 	for(j=i;j<=gap && key<arr[j-gap];j-=gap){
 				
-			}
-		}	
+		// 	}
+		// }
+		for(int i=gap;i<n;i++){
+        key = arr[i];
+        j = (i-gap);
+        while(j>=0 && key>arr[j]){
+            arr[j+gap] = arr[j];
+            j=j-gap;
+        }
+        arr[j+gap] = key;
+    }	
 	}
+	for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
 }
